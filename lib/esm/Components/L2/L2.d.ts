39daacd6f07c90296abcc9d637a0fc53060bd2ee
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-30 17:26:18 +0100

    rebuild

---
+++ lib/esm/Components/L2/L2.d.ts
@@ -0,0 +1,5 @@
+import React from "react";
+declare const L2: ({ style }: {
+    style?: React.CSSProperties | undefined;
+}) => JSX.Element;
+export default L2;
---
+++ lib/esm/Components/L4/L1.d.ts
@@ -0,0 +1,5 @@
+import React from "react";
+declare const L1: ({ style }: {
+    style?: React.CSSProperties | undefined;
+}) => JSX.Element;
+export default L1;
