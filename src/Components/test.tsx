ed1d429fa31a8e6840557343eace8fab29a1ae9a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-30 11:44:23 +0100

    add test component

---
+++ src/Components/test.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div></div>;
+};
+
+export default Test;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import test from "./Components/test";
+
 export function sayHello(name: string) {
   return `Hey ${name}, say hello to TypeScript.`;
 }
+
+export { test };
