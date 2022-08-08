d254880f0831ec8d105f073e922392fd6c7a0233
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-09 00:33:58 +0100

    Add storybook Fixes #1

---
+++ src/L.stories.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import * as L from "./index";
+
+export default {
+  title: "Components/Loaders",
+  component: L.L1,
+} as Meta;
+
+const style = {
+  height: "15rem",
+  width: "15rem",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+} as React.CSSProperties;
+
+export const L1 = () => <L.L1 style={style} />;
+export const L2 = () => <L.L2 style={style} />;
+export const L3 = () => <L.L3 style={style} />;
+export const L4 = () => <L.L4 style={style} />;
+export const L5 = () => <L.L5 style={style} />;
+export const L6 = () => <L.L6 style={style} />;
+export const L7 = () => <L.L7 style={style} />;
+export const L8 = () => <L.L8 style={style} />;
+export const L9 = () => <L.L9 style={style} />;
+export const L10 = () => <L.L10 style={style} />;
+export const L11 = () => <L.L11 style={style} />;
+export const L12 = () => <L.L12 style={style} />;
+export const L13 = () => <L.L13 style={style} />;
+export const L14 = () => <L.L14 style={style} />;
+export const L15 = () => <L.L15 style={style} />;
+export const L16 = () => <L.L16 style={style} />;
+export const L17 = () => <L.L17 style={style} />;
+export const L18 = () => <L.L18 style={style} />;
+export const L19 = () => <L.L19 style={style} />;
+export const L20 = () => <L.L20 style={style} />;
+export const L21 = () => <L.L21 style={style} />;
+export const L22 = () => <L.L22 style={style} />;
+export const L23 = () => <L.L23 style={style} />;
+export const L24 = () => <L.L24 style={style} />;
+export const L25 = () => <L.L25 style={style} />;
+export const L26 = () => <L.L26 style={style} />;
+export const L27 = () => <L.L27 style={style} />;
+export const L28 = () => <L.L28 style={style} />;
+export const L29 = () => <L.L29 style={style} />;
+export const L30 = () => <L.L30 style={style} />;
+export const L31 = () => <L.L31 style={style} />;
+export const L32 = () => <L.L32 style={style} />;
+export const L33 = () => <L.L33 style={style} />;
+export const L34 = () => <L.L34 style={style} />;
+export const L35 = () => <L.L35 style={style} />;
+export const L36 = () => <L.L36 style={style} />;
+export const L37 = () => <L.L37 style={style} />;
+export const L38 = () => <L.L38 style={style} />;
+export const L39 = () => <L.L39 style={style} />;
+export const L40 = () => <L.L40 style={style} />;
+export const L41 = () => <L.L41 style={style} />;
+export const L42 = () => <L.L42 style={style} />;
+export const L43 = () => <L.L43 style={style} />;
+export const L44 = () => <L.L44 style={style} />;
+export const L45 = () => <L.L45 style={style} />;
+export const L46 = () => <L.L46 style={style} />;
+export const L47 = () => <L.L47 style={style} />;
+export const L48 = () => <L.L48 style={style} />;
+export const L49 = () => <L.L49 style={style} />;
+export const L50 = () => <L.L50 style={style} />;export const L51 = () => <L.L51 style={style} />;
+export const L52 = () => <L.L52 style={style} />;
+export const L53 = () => <L.L53 style={style} />;
+export const L54 = () => <L.L54 style={style} />;
+export const L55 = () => <L.L55 style={style} />;
+export const L56 = () => <L.L56 style={style} />;
+export const L57 = () => <L.L57 style={style} />;
+export const L58 = () => <L.L58 style={style} />;
+export const L59 = () => <L.L59 style={style} />;
+export const L60 = () => <L.L60 style={style} />;
+export const L61 = () => <L.L61 style={style} />;
+export const L62 = () => <L.L62 style={style} />;
+export const L63 = () => <L.L63 style={style} />;
+export const L64 = () => <L.L64 style={style} />;
+export const L65 = () => <L.L65 style={style} />;
+export const L66 = () => <L.L66 style={style} />;
+export const L67 = () => <L.L67 style={style} />;
+export const L68 = () => <L.L68 style={style} />;
+export const L69 = () => <L.L69 style={style} />;
+export const L70 = () => <L.L70 style={style} />;
+export const L71 = () => <L.L71 style={style} />;
+export const L72 = () => <L.L72 style={style} />;
+export const L73 = () => <L.L73 style={style} />;
+export const L74 = () => <L.L74 style={style} />;
+export const L75 = () => <L.L75 style={style} />;
+export const L76 = () => <L.L76 style={style} />;
+export const L77 = () => <L.L77 style={style} />;
+export const L78 = () => <L.L78 style={style} />;
+export const L79 = () => <L.L79 style={style} />;
+export const L80 = () => <L.L80 style={style} />;
+export const L81 = () => <L.L81 style={style} />;
+export const L82 = () => <L.L82 style={style} />;
+export const L83 = () => <L.L83 style={style} />;
+export const L84 = () => <L.L84 style={style} />;
+export const L85 = () => <L.L85 style={style} />;
+export const L86 = () => <L.L86 style={style} />;
+export const L87 = () => <L.L87 style={style} />;
+export const L88 = () => <L.L88 style={style} />;
+export const L89 = () => <L.L89 style={style} />;
+export const L90 = () => <L.L90 style={style} />;
+export const L91 = () => <L.L91 style={style} />;
+export const L92 = () => <L.L92 style={style} />;
+export const L93 = () => <L.L93 style={style} />;
+export const L94 = () => <L.L94 style={style} />;
+export const L95 = () => <L.L95 style={style} />;
+export const L96 = () => <L.L96 style={style} />;
+export const L97 = () => <L.L97 style={style} />;
+export const L98 = () => <L.L98 style={style} />;
+export const L99 = () => <L.L99 style={style} />;
+export const L100 = () => <L.L100 style={style} />;
